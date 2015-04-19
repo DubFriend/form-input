50d3677342bfb77951fe4f792c1c775d77c71d38
Brian Detering <brian@Brians-MacBook-Pro.local>
2015-04-18 19:58:48 -0700

    groups

--- bootstrap-input.js
+++ bootstrap-input.js
@@ -288,32 +288,87 @@ var mixinPubSub = function (object) {
     return object;
 };
 
-var self = {
-	input: function (fig) {
-		var attr = fig.attr;
-		return '<input ' + filterUndefined(mapToArray(attr, function (val, key) {
-			if(key === 'value' && isObject(val)) {
-				if(
-					attr['name'] !== undefined &&
-					val[attr['name']] !== undefined
-				) {
-					return key + '="' + val[attr['name']] + '"';
-				}
-			}
-			else {
-				return val === undefined ? key : key + '="' + val +'"';
-			}
-		})).join(' ') + '/>';
-	}
+var bootstrapInput = function (fig) {
+    'use strict';
+
+    fig = fig || {};
+
+    var self = {};
+    
+    var feedbackOpen = fig.feedbackOpen  || '<span>';
+    var feedbackClose = fig.feedbackClose  || '</span>';
+    
+    var labelOpen = fig.labelOpen  || '<label>';
+    var labelClose = fig.labelClose  || '</label>';
+    
+    var groupOpen = fig.groupOpen  || '<div>';
+    var groupClose = fig.groupClose  || '</div>';
+
+    var groupControlOpen = fig.groupControlOpen  || '<div>';
+    var groupControlClose = fig.groupControlClose  || '</div>';
+
+    self.feedback = function (fig, name) {
+        if(isObject(fig)) {
+            if(fig[name]) {
+                return self.feedback(fig[name]);
+            }
+            else {
+                return '';
+            }
+        }
+        else if(isArray(fig)) {
+            return self.feedback('<ul>' + reduce(fig, function (carry, item) {
+                return carry + '<li>' + item + '</li>';
+            }, '') + '</ul>');
+        }
+        else {
+            return feedbackOpen + fig + feedbackClose;    
+        }
+    };
+    
+    self.input = function (attr) {
+        return '<input ' + filterUndefined(mapToArray(attr, function (val, key) {
+            if(key === 'value' && isObject(val)) {
+                if(
+                    attr.name !== undefined &&
+                    val[attr.name] !== undefined
+                ) {
+                    return key + '="' + val[attr['name']] + '"';
+                }
+            }
+            else {
+                return val === undefined ? key : key + '="' + val +'"';
+            }
+        })).join(' ') + '/>';
+    };
+
+    self.group = function (fig) {
+        var controlFig = fig.name ? union({ name: fig.name }, fig.input) : fig.input;
+        var controlText = self.input(controlFig);
+        var feedbackText = fig.feedback ? self.feedback(fig.feedback, fig.name) : '';
+        var labelText = fig.label ? labelOpen + fig.label + labelClose : '';
+
+        return groupOpen + 
+               labelText +
+               groupControlOpen +
+               controlText +
+               feedbackText +
+               groupControlClose +
+               groupClose;
+    };
+
+    return self;
+
 };
 
+
 if(typeof exports !== 'undefined') {
-	if(typeof module !== 'undefined' && module.exports) {
-		exports = module.exports = self;
-	}
-	exports.bootstrapInput = self;
+    if(typeof module !== 'undefined' && module.exports) {
+        exports = module.exports = bootstrapInput;
+    }
+    exports.bootstrapInput = bootstrapInput;
 } 
 else {
-	root.bootstrapInput = self;
+    root.bootstrapInput = bootstrapInput;
 }
 }).call(this);
--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
-var bootstrapInput = require('../bootstrap-input');
+var bootstrapInputGenerator = require('../bootstrap-input');
 exports.input = {
 	setUp: function (done) {
+		this.bootstrapInput = bootstrapInputGenerator();
 		done();
 	},
 
 	inputBasic: function (test) {
 		test.strictEqual(
-			bootstrapInput.input({ attr: { a: 'b' }}),
+			this.bootstrapInput.input({ a: 'b' }),
 			'<input a="b"/>'
 	    );
 		test.done();
@@ -14,7 +15,7 @@ exports.input = {
 
 	inputAttributeWithUndefinedValueDoesNotRenderValue: function (test) {
 		test.strictEqual(
-			bootstrapInput.input({ attr: { b: undefined } }),
+			this.bootstrapInput.input({ b: undefined }),
 			'<input b/>'
 	    );
 		test.done();
@@ -22,10 +23,10 @@ exports.input = {
 
 	inputPlucksValueAttributeFromName: function (test) {
 		test.strictEqual(
-			bootstrapInput.input({ attr: { 
+			this.bootstrapInput.input({ 
 				name: 'foo', 
 				value: { a: 'a', foo: 'bar' }
-			}}),
+			}),
 			'<input name="foo" value="bar"/>'
 		);
 		test.done();
@@ -33,11 +34,98 @@ exports.input = {
 
 	inputOmitsValueAttributeIfObjectAndCannotPluckFromName: function (test) {
 		test.strictEqual(
-			bootstrapInput.input({ attr: { 
+			this.bootstrapInput.input({ 
 				name: 'foo', 
 				value: { a: 'a', b: 'bar' }
-			}}),
+			}),
 			'<input name="foo"/>'
+		);
+		test.done();
+	},
+
+	feedbackPassedString: function (test) {
+		test.strictEqual(
+			this.bootstrapInput.feedback('foo'),
+			'<span>foo</span>'
+		);
+		test.done();
+	},
+
+	feedbackPassedArrayOfStrings: function (test) {
+		test.strictEqual(
+			this.bootstrapInput.feedback(['foo', 'bar']),
+			'<span><ul><li>foo</li><li>bar</li></ul></span>'
+		);
+		test.done();
+	},
+
+	feedbackPassedObjectWithNameKeyNotFound: function (test) {
+		test.strictEqual(
+			this.bootstrapInput.feedback({ bar: 'baz' }, 'foo'),
+			''
+		);
+		test.done();
+	},
+
+	feedbackPassedObjectWithNameKeyFoundString: function (test) {
+		test.strictEqual(
+			this.bootstrapInput.feedback({ foo: 'baz' }, 'foo'),
+			'<span>baz</span>'
+		);
+		test.done();
+	},
+
+	feedbackPassedObjectWithNameKeyFoundArray: function (test) {
+		test.strictEqual(
+			this.bootstrapInput.feedback({ foo: ['a', 'b'] }, 'foo'),
+			'<span><ul><li>a</li><li>b</li></ul></span>'
+		);
+		test.done();
+	},
+
+	feedbackCustomWrapperString: function (test) {
+		var bootstrapInput = bootstrapInputGenerator({
+			feedbackOpen: '{',
+			feedbackClose: '}'
+		});
+
+		test.strictEqual(
+			bootstrapInput.feedback('foo'),
+			'{foo}'
+		);
+		test.done();
+	},
+
+	feedbackCustomWrapperArray: function (test) {
+		var bootstrapInput = bootstrapInputGenerator({
+			feedbackOpen: '{',
+			feedbackClose: '}'
+		});
+
+		test.strictEqual(
+			bootstrapInput.feedback(['a', 'b']),
+			'{<ul><li>a</li><li>b</li></ul>}'
+		);
+		test.done();
+	},
+
+	inputGroupWithAName: function (test) {
+		test.strictEqual(
+			this.bootstrapInput.group({
+				label: 'bar',
+				name: 'foo',
+				input: {
+					value: { foo: 'b' }
+				},
+				feedback: { foo: 'c' }
+			}),
+			'<div>' +
+				'<label>bar</label>' +
+				'<div>' +
+					'<input name="foo" value="b"/>' +
+					'<span>c</span>' +
+				'</div>' +
+			'</div>'
 		);
 		test.done();
 	}
